677852996013ceb33ea284eaf0561bbfcf7b888d
noy4 <naoyan418@gmail.com>
2021-12-14 09:42:43 +0900

    env3

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+const basePath = isProd ? '/k-log-client' : ''
+
 module.exports = {
   reactStrictMode: true,
+  basePath,
+  assetPrefix: basePath,
+  env: {
+    basePath,
+  },
 }
